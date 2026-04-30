8078eb7f3a13052ba4ae68c2cb3b8ec689d2691c
Nathan <nathanbsb2@hotmail.com>
2026-04-30 18:20:19 -0300

    refactor(roles): update RoleEntity to match new schema

--- apps/api/src/domain/roles/entities/role.entity.ts
+++ apps/api/src/domain/roles/entities/role.entity.ts
@@ -1,18 +1,28 @@
 import { Expose } from "class-transformer";
-import { RoleType } from "../../../../prisma/generated/enums";
 import { RoleModel } from "../../../../prisma/generated/models/Role";
+import { UserEntity } from "../../users/entities/user.entity";
 
 export class RoleEntity implements RoleModel {
-	@Expose()
-	id: string;
+  @Expose()
+  id: string;
 
-	@Expose()
-	type: RoleType;
+  @Expose()
+  name: string;
 
-	@Expose()
-	userId: string;
+  @Expose()
+  createdAt: Date;
 
-	constructor(partial: Partial<RoleEntity>) {
-		Object.assign(this, partial);
-	}
+  @Expose()
+  updatedAt: Date;
+
+  @Expose()
+  users?: UserEntity[] | null;
+
+  constructor({ users, ...partial }: Partial<RoleEntity>) {
+    Object.assign(this, partial);
+
+    if (users) {
+      this.users = users.map((user) => new UserEntity(user));
+    }
+  }
 }
