4c79ece0668dc53b3ff1cf40ddd5802c5e6519a9
Nathan <nathanbsb2@hotmail.com>
2026-04-30 18:23:24 -0300

    fix(roles): remove circular UserEntity import from RoleEntity

--- apps/api/src/domain/roles/entities/role.entity.ts
+++ apps/api/src/domain/roles/entities/role.entity.ts
@@ -1,6 +1,5 @@
 import { Expose } from "class-transformer";
 import { RoleModel } from "../../../../prisma/generated/models/Role";
-import { UserEntity } from "../../users/entities/user.entity";
 
 export class RoleEntity implements RoleModel {
   @Expose()
@@ -15,14 +14,7 @@ export class RoleEntity implements RoleModel {
   @Expose()
   updatedAt: Date;
 
-  @Expose()
-  users?: UserEntity[] | null;
-
-  constructor({ users, ...partial }: Partial<RoleEntity>) {
+  constructor(partial: Partial<RoleEntity>) {
     Object.assign(this, partial);
-
-    if (users) {
-      this.users = users.map((user) => new UserEntity(user));
-    }
   }
 }
