ef97280fbd0dff4ffbb4c5f0823fcebdafa2b18f
Nathan <nathanbsb2@hotmail.com>
2026-04-30 18:31:39 -0300

    refactor(roles): update AssignRoleUseCase to use roleId instead of RoleType

--- apps/api/src/domain/roles/use-cases/assign-role.usecase.spec.ts
+++ apps/api/src/domain/roles/use-cases/assign-role.usecase.spec.ts
@@ -1,23 +1,20 @@
 import { beforeEach, describe, expect, it } from "@jest/globals";
-import { RoleEntity } from "../entities/role.entity";
 import { InMemoryRolesRepository } from "../repositories/in-memory-roles.repository";
 import { AssignRoleUseCase } from "./assign-role.usecase";
 
 describe("AssignRoleUseCase", () => {
-	let rolesRepository: InMemoryRolesRepository;
-	let sut: AssignRoleUseCase;
+  let rolesRepository: InMemoryRolesRepository;
+  let sut: AssignRoleUseCase;
 
-	beforeEach(() => {
-		rolesRepository = new InMemoryRolesRepository();
-		sut = new AssignRoleUseCase(rolesRepository);
-	});
+  beforeEach(() => {
+    rolesRepository = new InMemoryRolesRepository();
+    sut = new AssignRoleUseCase(rolesRepository);
+  });
 
-	it("should assign a role to a user", async () => {
-		const result = await sut.execute({ userId: "user-1", type: "USER" });
+  it("should assign a role to a user", async () => {
+    await sut.execute({ userId: "user-1", roleId: "role-1" });
 
-		expect(result).toBeInstanceOf(RoleEntity);
-		expect(result.userId).toBe("user-1");
-		expect(result.type).toBe("USER");
-		expect(rolesRepository.items).toHaveLength(1);
-	});
+    expect(rolesRepository.assignments).toHaveLength(1);
+    expect(rolesRepository.assignments[0]).toEqual({ userId: "user-1", roleId: "role-1" });
+  });
 });
--- apps/api/src/domain/roles/use-cases/assign-role.usecase.ts
+++ apps/api/src/domain/roles/use-cases/assign-role.usecase.ts
@@ -1,17 +1,11 @@
 import { Injectable } from "@nestjs/common";
-import { RoleType } from "../../../../prisma/generated/enums";
-import { RolesRepositoryProps } from "../repositories/roles.repository";
-
-interface AssignRoleInput {
-	userId: string;
-	type: RoleType;
-}
+import { AssignRoleData, RolesRepositoryProps } from "../repositories/roles.repository";
 
 @Injectable()
 export class AssignRoleUseCase {
-	constructor(private rolesRepository: RolesRepositoryProps) {}
+  constructor(private rolesRepository: RolesRepositoryProps) {}
 
-	async execute(data: AssignRoleInput) {
-		return this.rolesRepository.assign(data);
-	}
+  async execute(data: AssignRoleData): Promise<void> {
+    return this.rolesRepository.assign(data);
+  }
 }
